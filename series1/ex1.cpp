9c4ffc684f25e5614d37b0c3dfec8a518872abef
nikpag00 <nikospagonas00@gmail.com>
2021-03-20 20:04:17 +0200

    Add file input support

--- series1/ex1.cpp
+++ series1/ex1.cpp
@@ -1,1 +1,23 @@
-int main() {}
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+int main(int argc, char **argv) {
+  ifstream infile;
+  int M, N;
+  vector<int> v;
+  infile.open(argv[1]);
+  infile >> M;
+  infile >> N;
+
+  int a;
+  while (infile >> a) {
+    v.push_back(a);
+  }
+
+
+  // for (int i : v)
+  //   cout << i << ' ';
+  // cout << endl;
+}
