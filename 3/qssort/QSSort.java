66184a428a1373b7fc611a59fafc448d5548c93d
nikpag00 <nikospagonas00@gmail.com>
2021-07-02 12:45:59 +0300

    Implement file I/O for QSSort.java

--- 3/qssort/QSSort.java
+++ 3/qssort/QSSort.java
@@ -1,7 +1,27 @@
-import java.io.FileReader;
+import java.io.*;
 
 public class QSSort {
-  public static void main(String[] args) throws Exception {
-    FileReader fr = new FileReader(args[1]);
+  public static void main(String[] args) {
+    try {
+      BufferedReader in = new BufferedReader(new FileReader(args[0]));
+      String line = in.readLine();
+      String[] a = line.split(" ");
+      int N = Integer.parseInt(a[0]);
+      line = in.readLine();
+      String [] list_in_strings = line.split(" ");
+      int [] list = new int [N];
+
+      for (int i = 0; i < N; i++)
+        list[i] = Integer.parseInt(list_in_strings[i]);
+
+      in.close();
+
+      for (int i = 0; i < N; i++)
+        System.out.print(list[i] + " ");
+
+    }
+    catch(IOException e){
+      e.printStackTrace();
+    }
   }
 }
