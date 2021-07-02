9190ed51234bfe54e3ce323a32712bc250c17edd
nikpag00 <nikospagonas00@gmail.com>
2021-07-02 12:32:40 +0300

    Implement file I/O in Round

--- 3/round/Round.java
+++ 3/round/Round.java
@@ -3,18 +3,31 @@ import java.io.*;
 public class Round {
   public static void main (String[] args) {
     try {
+
+      //========================================================================
+      // Read from file.
+      //========================================================================
       BufferedReader in = new BufferedReader(new FileReader("r1.txt"));
       String line = in.readLine();
       String[] a = line.split(" ");
       int N = Integer.parseInt(a[0]);
       int K = Integer.parseInt(a[1]);
       line = in.readLine();
-      int [] initial = ;
+      String [] initial_in_strings = line.split(" ");
+      int [] initial = new int [K];
+
+      for (int i = 0; i < K; i++)
+        initial[i] = Integer.parseInt(initial_in_strings[i]);
 
       in.close();
 
       for (int i = 0; i < K; i++)
         System.out.print(initial[i] + " ");
+
+      //========================================================================
+      // Now, you can use N, K and initial as in ML.
+      //========================================================================
+
 
     }
     catch(IOException e) {
